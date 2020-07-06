fd4187ebc6c54073bc9785317e3d73737b427f91
traveltek-sveettem <somesh@traveltek.net>
2020-07-06 21:38:09 +0530

    First push

---
+++ js/custom-script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('#login-email').hide();
+    $('.again-login').hide();
+    $('.send-btn').hide();
+    $('.head-verification').hide();
+});
+
+function changeinput(){
+    $('#login-user').hide();
+    $('#login-password').hide();
+    $('#login-email').show();
+    $('.forget-pwd').hide();
+    $('.again-login').show();
+    $('.login-btn').hide();
+    $('.send-btn').show();
+    $('.head-verification').show();
+    $('.head-login').hide();
+}
+
+function changeinputlogin(){
+    $('#login-email').hide();
+    $('#login-user').show();
+    $('#login-password').show();
+    $('.forget-pwd').show();
+    $('.again-login').hide();
+    $('.login-btn').show();
+    $('.send-btn').hide();
+    $('.head-verification').hide();
+    $('.head-login').show();
+}
